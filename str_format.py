fb0389b15a1c90b9973c586ffd414ffe0fb2bcb4
陈文楷 <34624748+ccchen1996@users.noreply.github.com>
2019-04-05 23:27:28 +0800

    Delete str_format.py

--- str_format.py
+++
@@ -1,7 +0,0 @@
-age = 20
-name = 'Swaroop'
-
-print('{0} was {1} years old when he wrote this book'.format(name,age))
-print('Why is {0} playing with that python?'.format(name))
-#{}里的数字是可选项
-#name + 'is' + str(age) + 'years old'
